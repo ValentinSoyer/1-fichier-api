5f9c52869182a7599f142a324974c5dfdea956f6
Valentin <valent.soyer@gmail.com>
2021-02-09 19:26:16 +0100

    Update request_helper.rb

--- lib/un_fichier_api/helpers/request_helper.rb
+++ lib/un_fichier_api/helpers/request_helper.rb
@@ -6,16 +6,18 @@ module UnFichierApi
       extend ActiveSupport::Concern
 
       class_methods do
-        def action_route(action_name)
-          "#{self.name.demodulize.underscore}/#{action_name}.cgi"
-        end
+        private
+        
+          def action_route(action_name)
+            "#{self.name.demodulize.underscore}/#{action_name}.cgi"
+          end
 
-        def body_from_parameters(parameters:, method_binding:)
-          Hash[parameters.map do |type, arg|
-            next if method_binding.local_variable_get(arg).nil?
-            [arg, method_binding.local_variable_get(arg)]
-          end.compact]
-        end
+          def body_from_parameters(parameters:, method_binding:)
+            Hash[parameters.map do |type, arg|
+              next if method_binding.local_variable_get(arg).nil?
+              [arg, method_binding.local_variable_get(arg)]
+            end.compact]
+          end
       end
     end
   end
