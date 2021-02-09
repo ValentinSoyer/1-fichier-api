35b239dd62bb2058450d2e6c9f732cc356309459
Valentin <valent.soyer@gmail.com>
2021-02-09 18:48:18 +0100

    Finish folder api

--- lib/un_fichier_api/folder.rb
+++ lib/un_fichier_api/folder.rb
@@ -4,9 +4,29 @@ module UnFichierApi
   class Folder < Base
     # Methods
     class << self
+      def create(name: nil, folder_id: nil, sharing_user: nil)
+        body = body_from_parameters parameters: method(__method__).parameters, method_binding: binding
+        call path: action_route('mkdir'), body: body
+      end
+
       def list(folder_id: nil, sharing_user: nil, files: nil)
         body = body_from_parameters parameters: method(__method__).parameters, method_binding: binding
         call path: action_route('ls'), body: body
+      end
+
+      def move(folder_id: nil, destination_folder_id: nil, destination_user: nil, rename: nil)
+        body = body_from_parameters parameters: method(__method__).parameters, method_binding: binding
+        call path: action_route('mv'), body: body
+      end
+
+      def remove(folder_id: nil)
+        body = body_from_parameters parameters: method(__method__).parameters, method_binding: binding
+        call path: action_route('rm'), body: body
+      end
+
+      def share(folder_id: nil, share: nil, pass: nil, shares: [])
+        body = body_from_parameters parameters: method(__method__).parameters, method_binding: binding
+        call path: action_route('share'), body: body
       end
     end
   end
