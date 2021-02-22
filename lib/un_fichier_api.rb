4f6f5ab9afe45324a1107814149db73b5034a8ba
Valentin <valent.soyer@gmail.com>
2021-02-22 19:31:56 +0100

    Update un_fichier_api.rb

--- lib/un_fichier_api.rb
+++ lib/un_fichier_api.rb
@@ -3,7 +3,6 @@ require 'un_fichier_api/file'
 require 'un_fichier_api/folder'
 require 'un_fichier_api/ftp_user'
 require 'un_fichier_api/remote'
-require 'un_fichier_api/upload'
 require 'un_fichier_api/user'
 require 'un_fichier_api/version'
 
